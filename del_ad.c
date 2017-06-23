b27ffd97c107af838358446eb82b60cb8b5ff9f6
liuwen777 <beiligongliu@outlook.com>
2017-06-23 15:17:17 +0800

    Update del_ad.c

--- del_ad.c
+++ del_ad.c
@@ -23,8 +23,9 @@ int cgiMain()
 
 	int ret;
 	char sql[128] = "\0";
-	MYSQL *db;
 
+	 MYSQL *db;
+   sprintf(sql, "select * from  information where stuId ='%d'", atoi(stuId));
 	//初始化
 	db = mysql_init(NULL);
 	if (db == NULL)
@@ -41,15 +42,6 @@ int cgiMain()
 		mysql_close(db);
 		return -1;
 	}
-
-  /*if( statusdel == 0)
-	{
-	    sprintf(sql, "delete from information where stuId = %d", atoi(stuId));
-	}*/
-	//if( statusdel == 1 )
-	//{
-		  sprintf(sql, "delete from information where stuId = %d", atoi(stuId));
-
 	if ((ret = mysql_real_query(db, sql, strlen(sql) + 1)) != 0)
 	{
 		fprintf(cgiOut,"mysql_real_query fail:%s\n", mysql_error(db));
@@ -58,8 +50,30 @@ int cgiMain()
 	}
 
 
-	fprintf(cgiOut, "delete stu ok!\n");
-	mysql_close(db);
+	MYSQL_RES *res;
+	res = mysql_store_result(db);//mysql_store_result是一个不会导致任何伤害或性能降低的返回函数。
+	if (res == NULL)
+	{
+		fprintf(cgiOut,"mysql_srore_query fail:%s\n", mysql_error(db));
+		return -1;
+	}
 
+
+	int num=(int)res->row_count;
+	if(num>=1){
+       sprintf(sql, "delete from information  where stuId = %d", atoi(stuId));
+			 if ((ret = mysql_real_query(db, sql, strlen(sql) + 1)) != 0)
+		 	{
+		 		fprintf(cgiOut,"mysql_real_query fail:%s\n", mysql_error(db));
+		 		mysql_close(db);
+		 		return -1;
+		 	}
+				fprintf(cgiOut, "delete stu ok!\n");
+	}else{
+		fprintf(cgiOut, "此学生在数据库不存在!\n");
+	}
+
+	mysql_close(db);
 	return 0;
 }
+
